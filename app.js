45c09d83da7eef78ecfd1a8fa36e39f442fd15ca
mih777 <mih777777@yandex.ru>
2020-04-12 09:55:37 +0300

    for Heroku

--- app.js
+++ app.js
@@ -26,4 +26,17 @@ app.use(bodyParser.json())
 app.use('/api/todos', ToDoRouter)
 
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist/client'))
+
+  app.get('*', (req, res) => {
+      res.sendFile(
+          path.resolve(
+              __dirname, 'client', 'dist', 'client', 'index.html'
+          )
+      )
+  })
+}
+
+
 module.exports = app
